3a06462194e4a8940ce2cf1b3856c6c63ce5d95a
Ravi Sharma <sraa7877@gmail.com>
2024-03-26 21:42:35 +0530

    done

--- WeatherApp/src/main/java/com/RSstudies/servise/WeatherServise.java
+++ WeatherApp/src/main/java/com/RSstudies/servise/WeatherServise.java
@@ -1,15 +1,13 @@
-package com.RSstudies.servise;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.server.reactive.HttpHandler;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
-import org.springframework.web.bind.annotation.CrossOrigin; // Import CrossOrigin annotation
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -17,32 +15,31 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class WeatherServise {
 
-    @Autowired
-    private RestTemplate restTemplate;
-
-    private static final String XRapidAPIKey = "20ff479bccmsha2fc0220fbb7f2ap119675jsnada6d90b3a5f";
-    private static final String XRapidAPIHost= "forecast9.p.rapidapi.com";
-
-    @CrossOrigin(origins = "*") // Add CrossOrigin annotation with allowed origin
-    public Object getDataList(String country) {
-
-        try {
-            HttpHeaders headers = new HttpHeaders();
-            headers.set("X-RapidAPI-Key", XRapidAPIKey);
-            headers.set("X-RapidAPI-Host", XRapidAPIHost);
-            String url = "https://forecast9.p.rapidapi.com/rapidapi/forecast/"+country+"/summary/";
-
-            ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, new HttpEntity<>(headers),String.class);
-
-            return response.getBody();
-
-        } catch (Exception e) {
-            log.error("Something went wrong with Rapid API");
-            throw new ResponseStatusException(
-                    HttpStatus.INTERNAL_SERVER_ERROR,
-                    "Exception while calling endpoint ",
-                    e
-            );
-        }
-    }
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	private static final String XRapidAPIKey = "5e3134c76amsh5cc4891dcc4ecd6p104170jsnf6803a56aedb";
+	private static final String XRapidAPIHost= "forecast9.p.rapidapi.com";
+	
+	public Object getdataList(String country) {
+		
+		try {
+			HttpHeaders headers = new HttpHeaders();
+			headers.set("X-RapidAPI-Key", XRapidAPIKey);
+			headers.set("X-RapidAPI-Host", XRapidAPIHost);
+			String url = "https://forecast9.p.rapidapi.com/rapidapi/forecast/"+country+"/summary/";
+			
+			ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, new HttpEntity<>(headers),String.class);
+			
+			return response.getBody();
+			
+		} catch (Exception e) {
+			log.error("something went wrong with Rapid API");
+			throw new ResponseStatusException(
+					HttpStatus.INTERNAL_SERVER_ERROR,
+					"Exception while calling endpint ",
+					e
+					);
+		}
+	}
 }
